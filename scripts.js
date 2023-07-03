b4af077659428eb5d3bdd5094f3a02ce80bda4d7
Igor Melo <igormelo708@gmail.com>
2023-07-03 15:38:03 -0300

    ARQUIVOS NOVOS
    AAAAAAAAAAAAAA

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const ul = document.querySelector('ul')
+const input = document.querySelector('input')
+const form = document.querySelector('form')
+
+/*
+
+// Não se preocupem com esse pedaço de código comentado! Vamos descomentá-lo quando tivermos acabado de construir a API.
+
+// Função que carrega o conteúdo da API.
+async function load() {
+    // fetch está como await para evitar que entre num esquema de promisse e só devolva o conteúdo após a iteração qua acontece em seguida.
+    const res = await fetch('http://localhost:3000/')
+        .then(data => data.json())
+    // Iterando no vetor com o conteúdo (JSON) que está vindo da API e adicionando-os no frontend.
+    res.urls.map(({name, url}) => addElement({name, url}))
+}
+
+load()
+*/
+
+function addElement({ name, url }) {
+    
+}
+
+function removeElement(element) {
+    // criem os códigos
+}
+
+form.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+
+    let { value } = input
+
+    if (!value)
+        return alert('Preencha o campo!')
+
+    const [name, url] = value.split(',')
+
+    if (!url)
+        return alert('O texto não está formatado da maneira correta.')
+
+    if (!/^http/.test(url))
+        return alert('Digite a url da maneira correta.')
+
+    addElement({ name, url })
+
+    input.value = ''
+
+})
